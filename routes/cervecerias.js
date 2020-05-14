0080c1e90b587dbf39452745888bd9f1d9daaf15
Leandro Amaro Garcia <>
2020-05-14 11:25:06 -0300

    Agregado Metodo POST

--- routes/cervecerias.js
+++ routes/cervecerias.js
@@ -1,6 +1,6 @@
 const crudCervecerias = (app) => {
     const Cerveceria = require('../models/cervecerias.js');
-    
+
     //Funciones de endpoints
     //GET - Devuelve todas las cervecerias
     findAllCervecerias = (req, res) => {
@@ -11,8 +11,32 @@ const crudCervecerias = (app) => {
             }
         })
     }
+
+    //POST - Insert a new register in the DB
+    addCerveceria = function (req, res) {
+        console.log('POST');
+        console.log(req.body);
+        var cerveceria = new Cerveceria({
+            id: req.body.id,
+            lat: req.body.lat,
+            lng: req.body.lng,
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type
+        });
+        cerveceria.save(function (err) {
+            if (!err) {
+                console.log('Created');
+            } else {
+                console.log('ERROR: ' + err);
+            }
+        });
+        res.send(cerveceria);
+    };
+
     //URLS
-    app.get('/cervecerias', findAllCervecerias); 
+    app.get('/cervecerias', findAllCervecerias);
+    app.post('/cervecerias', addCerveceria);
 }
 
 module.exports = crudCervecerias;
