e673f33bc89a2744110822fc26cb530e7a4f1a6a
Leandro Amaro Garcia <>
2020-05-22 13:55:45 -0300

    test headers widthout cors

--- app.js
+++ app.js
@@ -19,8 +19,18 @@ app.use(bodyParser.json());
 app.use(methodOverride());
 var cors = require('cors')
 
+
 //app.use(cors())
-app.options('*', cors()) // include before other routes
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 routes = require('./routes/cervecerias')(app);
 
 app.get('/', function(req, res){
